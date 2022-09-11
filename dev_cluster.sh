d0c49dbd5a1d329f253be2160595212d53a741f4
JonZudell <jonathon.zudell@gmail.com>
2022-09-11 14:02:03 -0500

    dev_cluster minikube start

--- dev_cluster.sh
+++ dev_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-minikube up
+minikube start
 helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace --version 2.0
 helm upgrade --install spark microsoft/spark --namespace spark --create-namespace --version 1.0.4
